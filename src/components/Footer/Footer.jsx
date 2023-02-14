0e16fb0297db7f4142649602e9c8c51ca4784d19
Matias Agustin Lanfredi <matiaslanfredia@gmail.com>
2023-02-13 21:10:02 -0300

    Ecommerce full

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+import "./footer.css";
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="6" className="mb-4">
+            <div className="logo">
+              <div>
+                <h1 className="text-white">Incube</h1>
+              </div>
+            </div>
+            <p className="footer__text mt-4">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy
+            </p>
+          </Col>
+          <Col lg="3" md="3" className="mb-4">
+            <div className="footer__quick-links">
+              <h4 className="quick__link-title">Top Categories</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2" md="3" className="mb-4">
+            <div className="footer__quick-links">
+              <h4 className="quick__link-title">Useful Links</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3" md="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__link-title">Contact</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-item-center gap-2 ">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  <p>Calle falsa, 123, Buenos Aires</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-item-center gap-2 ">
+                  <span>
+                    <i class="ri-phone-line"></i>
+                  </span>
+                  <p>1122334455</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-item-center gap-2">
+                  <div className="btn-send">
+                    <span>
+                      <i class="ri-mail-send-line"></i>
+                    </span>
+                    <p>example@email.com</p>
+                    <button
+                      className="button__footer"
+                      onClick={() =>
+                        (window.location = "mailto:matiaslanfredia@gmail.com")
+                      }
+                    >
+                      Contact
+                    </button>
+                  </div>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="12">
+            <p className="footer__copyright">Copyrigth {year} </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+import products from "../assets/data/products";
+
+import Helmet from "../components/Helmet/Helmet";
+import Services from "../services/Services";
+import ProductsList from "../components/UI/ProductsList";
+import Clock from "../components/UI/Clock";
+
+import heroImg from "../assets/images/hero-img.png";
+import counterImg from "../assets/images/counter-timer-img.png";
+
+import "../styles/home.css";
+
+const Home = () => {
+  const year = new Date().getFullYear();
+
+  const [trendingProducts, setTrendingProducts] = useState([]);
+  const [bestSalesProducts, setBestSalesProducts] = useState([]);
+  const [mobileProducts, setMobileProducts] = useState([]);
+  const [wirelessProducts, setWirelessProducts] = useState([]);
+  const [popularProducts, setPopularProducts] = useState([]);
+
+  useEffect(() => {
+    const filteredTrendingProducts = products.filter(
+      (item) => item.category === "chair"
+    );
+    const filteredBestSalesProducts = products.filter(
+      (item) => item.category === "sofa"
+    );
+    const filteredMobileProducts = products.filter(
+      (item) => item.category === "mobile"
+    );
+    const filteredWirelessProducts = products.filter(
+      (item) => item.category === "wireless"
+    );
+    const filteredPopularProducts = products.filter(
+      (item) => item.category === "watch"
+    );
+    setTrendingProducts(filteredTrendingProducts);
+    setBestSalesProducts(filteredBestSalesProducts);
+    setMobileProducts(filteredMobileProducts);
+    setWirelessProducts(filteredWirelessProducts);
+    setPopularProducts(filteredPopularProducts);
+  }, []);
+
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending product in {year}</p>
+                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to="/shop">Shop Now</Link>
+                </motion.button>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Services />
+      <section className="trending__products">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Trending Products</h2>
+            </Col>
+            <ProductsList data={trendingProducts} />
+          </Row>
+        </Container>
+      </section>
+      <section className="best__sales">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Best Sales</h2>
+            </Col>
+            <ProductsList data={bestSalesProducts} />
+          </Row>
+        </Container>
+      </section>
+      <section className="timer__count">
+        <Container>
+          <Row>
+            <Col lg="6" md="12" className="count__down-col">
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6 mb-2">Limited Offers</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+              </div>
+              <Clock />
+              <motion.button
+                whileTap={{ scale: 1.2 }}
+                className="buy__btn store__btn"
+              >
+                <Link to="/shop">Visit Store</Link>
+              </motion.button>
+            </Col>
+            <Col lg="6" md="6" className="text-end counter__img">
+              <img src={counterImg} alt=""></img>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="new__arrivals">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <h2 className="section__title">New Arrivals</h2>
+            </Col>
+            <ProductsList data={mobileProducts} />
+            <ProductsList data={wirelessProducts} />
+          </Row>
+        </Container>
+      </section>
+      <section className="popular__category">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <h2 className="section__title">Popular in Category</h2>
+            </Col>
+            <ProductsList data={popularProducts} />
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Home;
